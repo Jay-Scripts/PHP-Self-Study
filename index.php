bf6dbb992408affa9d56b8c117b2abe4fed8d802
JayScriptt <corneliogatbontonjr21@gmail.com>
2025-06-04 23:37:38 +0800

    Day 2 PHP Progress Calculator program

--- index.php
+++ index.php
@@ -1,3 +1,36 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $operators = $_POST['operators'];
+    $ans = "    ";
+
+
+    if (empty($num2 = $_POST['num2']) || empty($num1 = $_POST['num1'])) {
+        $ans = "Please Input a Number!";
+    } else {
+        $num1 = intval($_POST['num1']);
+        $num2 = intval($_POST['num2']);
+
+        if ($operators == "+") {
+            $ans = $num1 + $num2;
+        } else if ($operators == "-") {
+            $ans = $num1 - $num2;
+        } else if ($operators == "/") {
+            if ($num2 == 0) {
+                $ans = "Cannot divide 0";
+            } else {
+                $ans = $num1 / $num2;
+            }
+        } else if ($operators == "%") {
+            $ans = $num1 % $num2;
+        } else if ($operators == "x") {
+            $ans = $num1 * $num2;
+        } else {
+            $ans = "Please Choose an Operator";
+        }
+    }
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -6,71 +39,36 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <script src="https://cdn.tailwindcss.com"></script>
-    <link rel="stylesheet" href="./src/input.css" />
 </head>
 
 <body>
-    <?php
-    $username = $password = $messageUsername = $messagePassword = $loginMsgSuccess = $loginMsgFail = "";
+    <div class="flex items-center justify-center flex-col h-screen">
+        <div class="border-2 border-black p-5 rounded-2xl bg-green-300 ">
+            <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+                <label for="num1">1st Number : </label><br>
+                <input type="number"
+                    name="num1"
+                    placeholder="Enter First Number : "><br>
+                <label for="num2">2nd Number : </label><br>
+                <input type="number"
+                    name="num2"
+                    placeholder="Enter Second Number : "><br>
+                <select name="operators" id="operatords">Choose Operators
+                    <option value="+">+</option>
+                    <option value="-">-</option>
+                    <option value="x">x</option>
+                    <option value="/">/</option>
+                    <option value="%">%</option>
+                </select><br>
+                <button name="submit">Submit</button>
 
+                <p><?php
+                    echo htmlspecialchars($ans);
+                    ?></p>
 
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        $username = $_POST['username'];
-        $password = $_POST['password'];
-        $sanitizedUserName = trim($username);
-        $sanitizedPassword = trim($password);
-        if ($sanitizedUserName === "Admin") {
-            $messageUsername = "Username Match!";
-        } else {
-            $messageUsername = "Username don't match!";
-        }
-
-        if ($sanitizedPassword == "12345") {
-            $messagePassword = "Password Match!";
-        } else {
-            $messagePassword = "Password don't match!";
-        }
-
-        if ($sanitizedUserName === "Admin" && $sanitizedPassword == "12345") {
-            $loginMsgSuccess = "Login successful, welcome admin!";
-        } else {
-            $loginMsgFail = "Invalid credentials";
-        }
-    }
-    ?>
-
-
-    <br><br><br><br><br>
-
-    <div class="flex items-center justify-center w-.5 h-0.4">
-        <div class="border-1 border-black p-4 rounded-3xl">
-            <form action="index.php" method="POST" class="text-center border-2 border-black p-5 rounded-3xl">
-                <p class="bg-green-300">
-                    <?php
-                    echo htmlspecialchars($loginMsgSuccess);
-                    ?>
-                </p>
-                <p class="bg-red-300">
-                    <?php
-                    echo htmlspecialchars($loginMsgFail);
-                    ?>
-                </p>
-                <label for="username">Username : </label>
-                <input class="m-2 border-2 border-black" type="text" name="username" value="<?php echo htmlspecialchars($username); ?>"><br>
-                <p class="bg-green-300"> <?php
-                                            echo htmlspecialchars($messageUsername);
-                                            ?></p>
-                <label for="password">Password :</label>
-                <input class="m-2 border-2 border-black" type="password" name="password" value="<?php echo htmlspecialchars($password); ?>"><br>
-                <p class="bg-green-300"> <?php
-                                            echo htmlspecialchars($messagePassword);
-                                            ?></p>
-                <br>
-                <button class="text-center bg-red-400 border-1 p-2 rounded-3xl" name="submit">Submit</button>
             </form>
         </div>
     </div>
-
 </body>
 
 </html>
