9f1916eb6cef6bb67c596a46ff667bd68961d1d4
JayScriptt <corneliogatbontonjr21@gmail.com>
2025-06-05 23:25:06 +0800

    PHP Day 3 Array Functions

--- index.php
+++ index.php
@@ -1,36 +1,3 @@
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $operators = $_POST['operators'];
-    $ans = "    ";
-
-
-    if (empty($num2 = $_POST['num2']) || empty($num1 = $_POST['num1'])) {
-        $ans = "Please Input a Number!";
-    } else {
-        $num1 = intval($_POST['num1']);
-        $num2 = intval($_POST['num2']);
-
-        if ($operators == "+") {
-            $ans = $num1 + $num2;
-        } else if ($operators == "-") {
-            $ans = $num1 - $num2;
-        } else if ($operators == "/") {
-            if ($num2 == 0) {
-                $ans = "Cannot divide 0";
-            } else {
-                $ans = $num1 / $num2;
-            }
-        } else if ($operators == "%") {
-            $ans = $num1 % $num2;
-        } else if ($operators == "x") {
-            $ans = $num1 * $num2;
-        } else {
-            $ans = "Please Choose an Operator";
-        }
-    }
-}
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -39,36 +6,22 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <script src="https://cdn.tailwindcss.com"></script>
+
 </head>
 
-<body>
-    <div class="flex items-center justify-center flex-col h-screen">
-        <div class="border-2 border-black p-5 rounded-2xl bg-green-300 ">
-            <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-                <label for="num1">1st Number : </label><br>
-                <input type="number"
-                    name="num1"
-                    placeholder="Enter First Number : "><br>
-                <label for="num2">2nd Number : </label><br>
-                <input type="number"
-                    name="num2"
-                    placeholder="Enter Second Number : "><br>
-                <select name="operators" id="operatords">Choose Operators
-                    <option value="+">+</option>
-                    <option value="-">-</option>
-                    <option value="x">x</option>
-                    <option value="/">/</option>
-                    <option value="%">%</option>
-                </select><br>
-                <button name="submit">Submit</button>
+<body class="h-screen flex justify-center items-center flex-col bg-gray-800 text-white">
+    <?php
+    $numbers = [1, 2, 4, 5, 76];
+    echo "sum of all numbers " . array_sum($numbers) . "<br>";
+    echo "maximum value " . max($numbers) . "<br>";
+    $desc = rsort($numbers);
 
-                <p><?php
-                    echo htmlspecialchars($ans);
-                    ?></p>
+    foreach ($numbers as $number) {
+        echo $number . "<br>";
+    }
+    ?>
 
-            </form>
-        </div>
-    </div>
+
 </body>
 
 </html>
