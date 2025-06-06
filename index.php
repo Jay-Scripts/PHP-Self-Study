9517af786938774cb10ad946201d8b64498fda20
JayScriptt <corneliogatbontonjr21@gmail.com>
2025-06-06 21:52:50 +0800

    Day 4 User Defined Functiion Activity

--- index.php
+++ index.php
@@ -11,79 +11,39 @@
 <body>
     <div class="flex items-center justify-center flex-col h-screen">
 
+        <?php
+        $name = $message = "";
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $name = $_POST['name'];
+        }
+        function greetUser($name)
+        {
+            $hour = date("H") + 8;
+            if ($hour < 12) {
+                $greeting = "Good Morning";
+            } elseif ($hour <  18) {
+                $greeting = "Good Afternoon";
+            } else {
+                $greeting = "Good Evening";
+            }
+            $message = "$greeting, $name! Welcome to our Website.";
 
+            echo $message;
+        }
+        ?>
         <div class="border-2 border-black p-2 rounded-2xl bg-blue-300  w-[300px] m-3">
-            <?php
-            $upperCase = "";
-            if ($_SERVER["REQUEST_METHOD"] == "POST") {
-                $upperCase = $_POST['upperCase'];
-            }
-            ?>
 
-            <form action="index.php" method="post" class=" flex justify-center items-center flex-col">
-                <p class="text-center">Text Inputed here will turn into Uppercase</p>
-                <label for="upperCase">Enter Text :</label><br>
-                <input type="text" name="upperCase"> <br>
-                <button name="submit" class="p-3 text-center border-2 rounded-2xl m-2">Convert</button>
+            <form action="index.php" method="post" class="flex flex-col items-center justify-center gap-2  text-white">
+                <label for="name">Input your Name : </label>
+                <input type="text" name="name" id="name">
+                <button name="submit">Submit</button>
                 <p><?php
-                    echo strtoupper($upperCase);
+                    echo greetUser($name);
                     ?></p>
             </form>
-        </div>
-        <div class="border-2 border-black p-2 rounded-2xl bg-red-300  w-[300px] m-3">
-            <?php
-            $strpos = $result = "";
 
-            if ($_SERVER["REQUEST_METHOD"] == "POST") {
-                $strpos = $_POST['strpos'];
-                $result = strpos($strpos, "a");
-            }
-            ?>
-            <form action="index.php" method="post" class=" flex justify-center items-center flex-col">
-                <p class="text-center">Text Inputed here will find the letter a</p>
-                <label for="strpos">Enter Text with a :</label><br>
-                <input type="text" name="strpos"> <br>
-                <button name="submit" class="p-3 text-center border-2 rounded-2xl m-2">Find</button>
-                <p><?php
-                    echo $result;
-                    ?></p>
-            </form>
         </div>
-        <div class="border-2 border-black p-2 rounded-2xl bg-yellow-300  w-[300px] m-3">
-            <?php
-            $substr = $extracted = "";
-            if ($_SERVER["REQUEST_METHOD"] == "POST") {
-                $substr = $_POST['substr'];
-                $extracted = substr($substr, 2, 5);
-            }
-            ?>
-            <form action="index.php" method="post" class=" flex justify-center items-center flex-col">
-                <p class="text-center">Text Inputed here will be extracted starting from index 2 with length 5</p>
-                <label for="substr">Enter Text with a :</label><br>
-                <input type="text" name="substr"> <br>
-                <button name="submit" class="p-3 text-center border-2 rounded-2xl m-2">Extract</button>
-                <p><?php
-                    echo $extracted;
-                    ?></p>
-            </form>
-        </div>
-        <div class="border-2 border-black p-2 rounded-2xl bg-green-300  w-[300px] m-3">
-            <?php
-            $reverse  = "";
-            if ($_SERVER["REQUEST_METHOD"] == "POST") {
-                $reverse = $_POST['reverse'];
-            }
-            ?>
-            <form action="index.php" method="post" class=" flex justify-center items-center flex-col">
-                <p class="text-center">Text Inputed here will be reverserd </p>
-                <label for="reverse">Enter Text with a :</label><br>
-                <input type="text" name="reverse"> <br>
-                <button name="submit" class="p-3 text-center border-2 rounded-2xl m-2">Reverse</button>
-                <p><?php
-                    echo strrev($reverse);
-                    ?></p>
-            </form>
-        </div>
+
     </div>
 
 
